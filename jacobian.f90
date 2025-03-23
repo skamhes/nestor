9ed5a31ba925deb532491924cf13d6cdd8169843
skamhes <karsten.hendrickson@gmail.com>
2025-03-23 16:01:17 -0400

    Fixed bug that caused the viscous flux jacobian to only be computed for inviscid flows in boundary flux...

--- jacobian.f90
+++ jacobian.f90
@@ -117,7 +117,7 @@ module jacobian
                 ! We only have a diagonal term to add
                 jac(c1)%diag            = jac(c1)%diag            + dFnduL * face_mag
 
-                if ( iflow_type > FLOW_INVISCID ) cycle bfaces_loop
+                if ( iflow_type == FLOW_INVISCID ) cycle bfaces_loop
 
                 face_sides = bound(ib)%bfaces(1,i)
 
