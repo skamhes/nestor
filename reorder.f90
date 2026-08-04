9d9641807c0d6b50023372cd95a6ddfe5c8b266b
Karsten Hendrickson <karsten.hendrickson@gmail.com>
2026-08-04 19:49:17 -0400

    Reworked the face reordering to improve bandwidth of flux loops.
    Faces are reordered so that the attached cells are (roughly increasing).
    Should improve cache hits by limiting the iterations between the first
    and last read during face loops.

--- reorder.f90
+++ reorder.f90
@@ -11,26 +11,39 @@ module reorder
 
     subroutine reorder_rcm
 
+        use common , only : p2
+
         use config , only : rcm_verbosity
         
-        use grid , only : cc_data_type, bgrid_type, ncells, cell, face, nfaces, nb, bound, gcell, build_ghost_cells
+        use grid , only : cc_data_type, bgrid_type, ncells, cell, face, nfaces, nb, bound, gcell, build_ghost_cells, &
+                          face_centroid, face_nrml, face_nrml_mag
 
         use sort_routines , only : inserstion_sort_ind
 
         implicit none
 
-        type(cc_data_type), dimension(:), pointer :: rcm_cell ! reordered array
+        type fc
+            integer, dimension(:), allocatable :: iface    ! index of faces
+            integer                            :: nf       ! number of faces
+        end type fc
+
+        type(cc_data_type), dimension(:)  , pointer :: rcm_cell ! reordered array
+        integer,            dimension(:,:), pointer :: rcm_face ! reordered face array
+        real(p2),           dimension(:,:), pointer :: rcm_fc, rcm_fn ! other reordered face array
+        real(p2),           dimension(:)  , pointer :: rcm_fa
 
         integer :: min_deg, imd, max_deg ! index of the cell with the minimum degree (# of neighbors)
         integer :: i, j, ib, ihead, iqueue, phead
         integer :: nadj, cn, iadj
         integer :: cL, cR, cLq, cRq
+        integer :: fn, nface_loc
 
         integer, dimension(:,:), allocatable :: adj
 
         integer, dimension(ncells) :: queue
         
         integer, dimension(ncells) :: c2q ! convert input array to queue
+        type(fc),dimension(ncells) :: c2f ! converts cell faces to face array index
 
         write(*,*) "Reordering mesh using Reverse Cuthill-Mckee"
 
@@ -128,7 +141,17 @@ module reorder
 
         ! update face array
 
+        allocate(rcm_face(2,ncells)) ! only store the cells.
+        
+        do i = 1,ncells
+            allocate( c2f(i)%iface(cell(i)%nnghbrs) )
+            c2f(i)%nf = 0
+        end do
+
+        
+
         do i = 1,nfaces
+            ! first make sure the face is pointing to the updated cell indices
             cL  = face(1,i)
             cR  = face(2,i)
             cLq = c2q(cL)
@@ -136,8 +159,39 @@ module reorder
 
             face(1,i) = cLq
             face(2,i) = cRq
+
+            ! now grab that data to make the association struct
+            cn = minval(face(1:2,i)) ! smaller number face
+            c2f(cn)%nf = c2f(cn)%nf + 1
+            c2f(cn)%iface(c2f(cn)%nf) = i
         end do
 
+        allocate(rcm_face(2,nfaces))
+        allocate(rcm_fc(3,nfaces))
+        allocate(rcm_fn(3,nfaces))
+        allocate(rcm_fa(nfaces))
+        
+        nface_loc = 0
+        do i = 1,ncells
+            do j = 1,c2f(i)%nf
+                fn = c2f(i)%iface(j)
+                nface_loc = nface_loc + 1
+                rcm_face(1:2,nface_loc) = face(1:2,fn)
+                rcm_fc(:,nface_loc) = face_centroid(:,fn)
+                rcm_fn(:,nface_loc) = face_nrml(:,fn)
+                rcm_fa(nface_loc)   = face_nrml_mag(fn)
+            end do
+            deallocate( c2f(i)%iface )
+        end do
+
+        deallocate(face, face_centroid, face_nrml, face_nrml_mag)
+        face          => rcm_face
+        face_centroid => rcm_fc
+        face_nrml     => rcm_fn
+        face_nrml_mag => rcm_fa
+
+        nullify(rcm_face, rcm_fc, rcm_fn, rcm_fa)
+        
         ! Update boundary array
         do ib = 1,nb
             do i = 1,bound(ib)%nbfaces
